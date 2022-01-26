3b998cc04ff4ae097727fe0fcff11239860fac7f
ashwing97 <ashwingg97@gmail.com>
2022-01-26 12:10:24 -0600

    Third commit

---
+++ Subtraction/src/sub.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the numbers: ");
+		int a = in.nextInt();
+		int b = in.nextInt();
+		System.out.println("Subtraction is :"+(a-b));
+		
+	}
+
+}
