6938d97ac63c22b94a8c43667ee815d0210c4534
ashwi <ashwi@LAPTOP-45F67UJT.attlocal.net>
2022-01-26 11:53:36 -0600

    second commit today

---
+++ Addition/src/add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Enter numbers: ");
+		a = myScanner.nextInt();
+		b = myScanner.nextInt();
+		System.out.println("Addition is :"+(a+b));
+
+	}
+
+	
+}
